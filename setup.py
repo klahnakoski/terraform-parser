467d8a9342f687095e47d34fed1ccde9f95dcd15
Kyle Lahnakoski <kyle@lahnakoski.com>
2022-12-27 22:04:37 -0500

    update version number

--- setup.py
+++ setup.py
@@ -16,6 +16,6 @@ setup(
     name='terraform-parser',
     packages=["terraform_parser"],
     url='https://github.com/klahnakoski/terraform-parser',
-    version='9.317.22362',
+    version='9.319.22362',
     zip_safe=False
 )
