4ac7c36a77164bc4313dc5ffcb49d5aab199d095
Kyle Lahnakoski <kyle@lahnakoski.com>
2023-01-10 07:42:05 -0500

    updates from other projects

--- tests/test_diagram.py
+++ tests/test_diagram.py
@@ -1,4 +1,17 @@
+# encoding: utf-8
+#
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this file,
+# You can obtain one at http://mozilla.org/MPL/2.0/.
+#
+# Contact: Kyle Lahnakoski (kyle@lahnakoski.com)
+#
+from unittest import TestCase
 
+
+class TestDiagram(TestCase):
+
+    def test_simple(self):
 
 # LOAD THE TERRAFORM
 
